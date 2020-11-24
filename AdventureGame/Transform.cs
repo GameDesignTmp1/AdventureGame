3b2ddd228ac3c03a3c87bd2adda3b9f28796c92c
1363558876@qq.com <1363558876@qq.com>
2020-11-24 16:53:16 +0800

    添加说明

---
+++ AdventureGame/Transform.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    public class Transform : ITransform
+    {
+        public Vec2 Location = new Vec2();
+        public bool ToLeft = false;
+        public GameObject GameObject;
+        public Collision collision;
+
+        public Transform(GameObject gameObject)
+        {
+            GameObject = gameObject;
+            collision = gameObject.collision;
+        }
+
+        public Transform(GameObject gameObject, Vec2 location, bool turnLeft)
+        {
+            Location = location;
+            ToLeft = turnLeft;
+            GameObject = gameObject;
+            collision = gameObject.collision;
+        }
+
+        public void Translate(Vec2 dir)
+        {
+            var d = collision.GetMoveDis(dir);
+            GameObject.X += d.X;
+            GameObject.Y += d.Y;
+        }
+
+        public void TurnLeft()
+        {
+            ToLeft = true;
+        }
+
+        public void TurnRight()
+        {
+            ToLeft = false;
+        }
+    }
+}
---
+++ AdventureGame/Vec2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    public class Vec2
+    {
+        public double X, Y;
+
+        public Vec2()
+        {
+            X = Y = 0;
+        }
+        public Vec2(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Square()
+        {
+            return X * X + Y * Y;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(Square());
+        }
+
+        public static Vec2 operator /(Vec2 v, double d)
+        {
+            var dd = 1 / d;
+            return new Vec2(v.X * d, v.Y * d);
+        }
+
+        public Vec2 Normalize()
+        {
+            return this / Length();
+        }
+    }
+}
